7d9ed51b967886008a56730ff7a98da875056d95
iker <iker@Z370-AORUS>
2023-03-11 12:16:44 +0100

    Add project files.

---
+++ AsterixDecoder/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+
+namespace AsterixDecoder
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            CAT10 cAT10 = new CAT10();
+            string hex = "2f";
+            label1.Text = cAT10.HexToBinary(hex);
+        }
+    }
+}
---
+++ ClassLibrary/CAT10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class CAT10
+    {
+        public string HexToBinary(string hexadecimal)
+        {
+            string binary = Convert.ToString(Convert.ToInt32(hexadecimal, 16), 2).PadLeft(hexadecimal.Length*4, '0');
+            return binary;
+        }
+    }
+}
